7c0100badeca524b1c6d336bb23c6c5144807ec2
Gabriel Ferreira <gabriel.ferreira.itba@gmail.com>
2021-04-19 18:58:36 -0300

    feat: estilizando dashboard

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,24 @@
+import { VscSignOut } from 'react-icons/vsc';
+
 import * as S from './styles';
 
+import logoImg from '../../assets/logo.png';
+
 const Header = () => {
-  return <S.Wrapper></S.Wrapper>;
+  return (
+    <S.Wrapper>
+      <img src={logoImg} alt="Imagem do logo" />
+
+      <div>
+        <h4>Seja bem-vindo,</h4>
+        <h3>Gabriel Ferreira</h3>
+      </div>
+
+      <button>
+        <VscSignOut size={30} color="#666360" />
+      </button>
+    </S.Wrapper>
+  );
 };
 
 export default Header;
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,3 +1,28 @@
 import styled from 'styled-components';
 
-export const Wrapper = styled.header``;
+export const Wrapper = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100vw;
+  height: 90px;
+  padding: 0 15rem;
+
+  box-shadow: 0px 0px 3px rgba(0, 0, 0, 0.4);
+
+  img {
+    transform: scale(0.8);
+  }
+
+  > div {
+    margin-right: 450px;
+
+    > h3 {
+      color: #ff9000;
+    }
+  }
+  > button {
+    background: none;
+    border: none;
+  }
+`;
