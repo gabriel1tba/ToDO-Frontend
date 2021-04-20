669170ce0743e1430f24dbbee025f8abedf0b747
Gabriel Ferreira <gabriel.ferreira.itba@gmail.com>
2021-04-20 16:05:29 -0300

    feat: adicionando variaveis de ambiente

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3333',
+  baseURL: `${process.env.REACT_APP_API_HOST}:${process.env.REACT_APP_API_PORT}`,
   headers: {
     'Content-Type': 'application/json;charset=UTF-8',
     Accept: 'application/json',
