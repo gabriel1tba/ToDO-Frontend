dae933f702a53e6528a743962032da5c3b1e2334
Gabriel Ferreira <gabriel.ferreira.itba@gmail.com>
2021-04-24 10:17:25 -0300

    refactor: dashboard icon message

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,4 @@
-import { AiOutlineLogout } from 'react-icons/ai';
+import { VscSignOut } from 'react-icons/vsc';
 
 import * as S from './styles';
 
@@ -24,7 +24,7 @@ const Header = ({ userName, handleSignOut }: IHeader) => {
       </div>
 
       <button onClick={handleSignOut}>
-        <AiOutlineLogout size={40} color="#666360" />
+        <VscSignOut size={40} color="#666360" />
       </button>
     </S.Wrapper>
   );
