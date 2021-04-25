14dfb1199f0608e8b30eaf011e62c75b42c7d0d0
Gabriel Ferreira <gabriel.ferreira.itba@gmail.com>
2021-04-25 11:18:29 -0300

    feat: função handleCompletedTodo

--- src/components/TodoContainer/Todo/index.tsx
+++ src/components/TodoContainer/Todo/index.tsx
@@ -1,4 +1,8 @@
+import { useCallback } from 'react';
 import { FaEdit, FaTrashAlt } from 'react-icons/fa';
+import { useToast } from '../../../hooks/toast';
+import { useTodos } from '../../../hooks/todos';
+import api from '../../../services/api';
 
 import * as S from './styles';
 
@@ -17,6 +21,32 @@ interface ITodo {
 }
 
 const Todo = ({ todo }: ITodo) => {
+  const { addToast } = useToast();
+  const { handleGetTodos } = useTodos();
+
+  const handleCompletedTodo = useCallback(
+    (checked: boolean) => {
+      todo.completed = checked;
+
+      try {
+        api.patch('/todos', {
+          ...todo,
+        });
+      } catch (error) {
+        addToast({
+          type: 'error',
+          title: 'Erro ao fazer alteração.',
+          description:
+            'Um erro ocorreu ao marcar tarefa como completa. Tente novamente mais tarde.',
+          secondsDuration: 5,
+        });
+      }
+
+      handleGetTodos();
+    },
+    [addToast, handleGetTodos, todo],
+  );
+
   return (
     <S.Wrapper>
       <input
@@ -24,6 +54,7 @@ const Todo = ({ todo }: ITodo) => {
         type="checkbox"
         name="completed"
         id="completed"
+        onChange={(event) => handleCompletedTodo(event.target.checked)}
       />
 
       <a onClick={() => alert(todo.title)}>{todo.title}</a>
