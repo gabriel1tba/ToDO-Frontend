91c6bde112f99362d5430de7b84f2151e766ceee
Gabriel Ferreira <gabriel.ferreira.itba@gmail.com>
2021-04-28 08:58:30 -0300

    feat: alterando tamanho de fontes

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -14,7 +14,7 @@ export const GlobalStyles = createGlobalStyle`
     --webkit-font-smoothing: antialiased;
   }
 
-  body, input, textarea,button{
+  body, input, textarea, button {
     font-family: 'Roboto', sans-serif;  }
 
   h1, h2, h3, h4, h5, h6, strong {
