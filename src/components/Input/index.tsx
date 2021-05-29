3c2f0f12ff93bcbd1e61b855e4f096dc3a84d408
Gabriel Ferreira <gabriel.ferreira.itba@gmail.com>
2021-05-29 10:42:11 -0300

    feat: input to react-hook-form

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,5 +1,4 @@
-import { useCallback, useEffect, useRef, useState } from 'react';
-import { useField } from '@unform/core';
+import { forwardRef, useCallback, useRef, useState } from 'react';
 import { IconBaseProps } from 'react-icons';
 import { FiAlertCircle } from 'react-icons/fi';
 
@@ -8,51 +7,51 @@ import * as S from './styles';
 interface IInput extends React.InputHTMLAttributes<HTMLInputElement> {
   name: string;
   icon?: React.ComponentType<IconBaseProps>;
+  ref: React.LegacyRef<HTMLInputElement> | undefined;
+  error: string | undefined;
+  defaultValue?: string | number | string[] | undefined;
 }
 
-const Input = ({ name, icon: Icon, ...rest }: IInput) => {
-  const [isFocused, setIsFocued] = useState(false);
-  const [isFilled, setIsFilled] = useState(false);
+// eslint-disable-next-line react/display-name
+const Input = forwardRef(
+  ({ name, icon: Icon, error, defaultValue = '', ...rest }: IInput, ref) => {
+    const [isFocused, setIsFocued] = useState(false);
+    const [isFilled, setIsFilled] = useState(false);
 
-  const inputRef = useRef({} as HTMLInputElement);
+    const inputRef = useRef({} as HTMLInputElement);
 
-  const { fieldName, error, registerField, defaultValue } = useField(name);
+    const handleInputFocus = useCallback(() => {
+      setIsFocued(true);
+    }, []);
 
-  const handleInputFocus = useCallback(() => {
-    setIsFocued(true);
-  }, []);
+    const handleInputBlur = useCallback(() => {
+      setIsFocued(false);
 
-  const handleInputBlur = useCallback(() => {
-    setIsFocued(false);
+      inputRef.current.value ? setIsFilled(true) : setIsFilled(false);
+    }, []);
 
-    inputRef.current.value ? setIsFilled(true) : setIsFilled(false);
-  }, []);
-
-  useEffect(() => {
-    registerField({
-      name: fieldName,
-      ref: inputRef.current,
-      path: 'value',
-    });
-  }, [fieldName, registerField]);
-
-  return (
-    <S.Wrapper hasError={!!error} isFocused={isFocused} isFilled={isFilled}>
-      {Icon && <Icon size={20} />}
-      <input
-        defaultValue={defaultValue}
-        onFocus={handleInputFocus}
-        onBlur={handleInputBlur}
-        ref={inputRef}
-        {...rest}
-      />
-      {error && (
-        <S.Error title={error}>
-          <FiAlertCircle color={isFocused ? '#ff9000' : '#c43030'} size={20} />
-        </S.Error>
-      )}
-    </S.Wrapper>
-  );
-};
+    return (
+      <S.Wrapper hasError={!!error} isFocused={isFocused} isFilled={isFilled}>
+        {Icon && <Icon size={20} />}
+        <input
+          defaultValue={defaultValue}
+          onFocus={handleInputFocus}
+          onBlur={handleInputBlur}
+          ref={ref}
+          name={name}
+          {...rest}
+        />
+        {error && (
+          <S.Error title={error}>
+            <FiAlertCircle
+              color={isFocused ? '#ff9000' : '#c43030'}
+              size={20}
+            />
+          </S.Error>
+        )}
+      </S.Wrapper>
+    );
+  },
+);
 
 export default Input;
