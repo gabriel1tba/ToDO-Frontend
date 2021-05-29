0982ec1faed6e38e0ea3c61d2ad5e685f5cef935
Gabriel Ferreira <gabriel.ferreira.itba@gmail.com>
2021-05-29 10:46:31 -0300

    feat: textarea to react-hook-form

--- src/components/TextArea/index.tsx
+++ src/components/TextArea/index.tsx
@@ -1,55 +1,54 @@
-import { useCallback, useEffect, useRef, useState } from 'react';
-import { useField } from '@unform/core';
+/* eslint-disable react/display-name */
+import { forwardRef, useCallback, useRef, useState } from 'react';
 import { FiAlertCircle } from 'react-icons/fi';
 
 import * as S from './styles';
 
 interface ITextArea extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {
   name: string;
+  ref: React.LegacyRef<HTMLTextAreaElement> | undefined;
+  error: string | undefined;
+  defaultValue?: string | number | string[] | undefined;
 }
 
-const TextArea = ({ name, ...rest }: ITextArea) => {
-  const [isFocused, setIsFocued] = useState(false);
-  const [isFilled, setIsFilled] = useState(false);
+const TextArea = forwardRef(
+  ({ name, error, defaultValue = '', ...rest }: ITextArea, ref) => {
+    const [isFocused, setIsFocued] = useState(false);
+    const [isFilled, setIsFilled] = useState(false);
 
-  const textAreaRef = useRef({} as HTMLTextAreaElement);
+    const textAreaRef = useRef({} as HTMLTextAreaElement);
 
-  const { fieldName, error, registerField, defaultValue } = useField(name);
+    const handleInputFocus = useCallback(() => {
+      setIsFocued(true);
+    }, []);
 
-  const handleInputFocus = useCallback(() => {
-    setIsFocued(true);
-  }, []);
+    const handleInputBlur = useCallback(() => {
+      setIsFocued(false);
 
-  const handleInputBlur = useCallback(() => {
-    setIsFocued(false);
+      textAreaRef.current.value ? setIsFilled(true) : setIsFilled(false);
+    }, []);
 
-    textAreaRef.current.value ? setIsFilled(true) : setIsFilled(false);
-  }, []);
-
-  useEffect(() => {
-    registerField({
-      name: fieldName,
-      ref: textAreaRef.current,
-      path: 'value',
-    });
-  }, [fieldName, registerField]);
-
-  return (
-    <S.Wrapper hasError={!!error} isFocused={isFocused} isFilled={isFilled}>
-      <textarea
-        defaultValue={defaultValue}
-        onFocus={handleInputFocus}
-        onBlur={handleInputBlur}
-        ref={textAreaRef}
-        {...rest}
-      />
-      {error && (
-        <S.Error title={error}>
-          <FiAlertCircle color={isFocused ? '#ff9000' : '#c43030'} size={20} />
-        </S.Error>
-      )}
-    </S.Wrapper>
-  );
-};
+    return (
+      <S.Wrapper hasError={!!error} isFocused={isFocused} isFilled={isFilled}>
+        <textarea
+          defaultValue={defaultValue}
+          onFocus={handleInputFocus}
+          onBlur={handleInputBlur}
+          ref={ref}
+          name={name}
+          {...rest}
+        />
+        {error && (
+          <S.Error title={error}>
+            <FiAlertCircle
+              color={isFocused ? '#ff9000' : '#c43030'}
+              size={20}
+            />
+          </S.Error>
+        )}
+      </S.Wrapper>
+    );
+  },
+);
 
 export default TextArea;
