bc1c8d56a1113c59a50e9519ab0240d773b17eb9
Gabriel Ferreira <gabriel.ferreira.itba@gmail.com>
2021-06-01 16:54:57 -0300

    feat: reducer Todos

---
+++ src/hooks/todos/reducer.ts
@@ -0,0 +1,40 @@
+import { ActionType, TodoActions } from './actions';
+import { ITodo } from './interfaces';
+
+const todoReducer = (state: ITodo[], action: TodoActions): ITodo[] => {
+  switch (action.type) {
+    case ActionType.CreateTodo: {
+      return [...state, action.payload];
+    }
+
+    case ActionType.UpdateTodo: {
+      return state
+        .sort(
+          (a: ITodo, b: ITodo) =>
+            new Date(a.created_at).getTime() - new Date(b.created_at).getTime(),
+        )
+        .map((todo) => {
+          if (action.payload.id === todo.id) {
+            todo = action.payload;
+          }
+          return todo;
+        });
+    }
+
+    case ActionType.DeleteTodo: {
+      return state
+        .filter((todo) => {
+          return todo !== action.payload;
+        })
+        .sort(
+          (a: ITodo, b: ITodo) =>
+            new Date(a.created_at).getTime() - new Date(b.created_at).getTime(),
+        );
+    }
+
+    default:
+      return state;
+  }
+};
+
+export default todoReducer;
