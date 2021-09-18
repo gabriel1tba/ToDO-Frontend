2575c1a4421a497e212033767a5a7a8e795d0e13
Gabriel Ferreira <gabriel.ferreira.itba@gmail.com>
2021-09-18 13:52:33 -0300

    feat: tornando header responsivo

--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -10,5 +10,21 @@ export const Wrapper = styled.div`
 
     margin-top: 80px;
     margin-left: 340px;
+
+    @media (max-width: 1024px) {
+      margin-left: 0;
+    }
+
+    @media (max-width: 768px) {
+      margin-left: 0;
+    }
+
+    @media (max-width: 540px) {
+      margin-left: 0;
+    }
+
+    @media (max-width: 360px) {
+      margin-left: 0;
+    }
   }
 `;
