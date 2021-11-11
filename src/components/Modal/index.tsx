fb0842a7a68d6012510032726ca1c9be79fe6d13
Gabriel Ferreira <gabriel.ferreira.itba@gmail.com>
2021-11-11 18:02:08 -0300

    feat: renderizando modal com createPortal

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,3 +1,4 @@
+import ReactDOM from 'react-dom';
 import { MdClose } from 'react-icons/md';
 
 import * as S from './styles';
@@ -9,23 +10,24 @@ interface IModal {
   handleCloseModal: () => void;
 }
 
-const Modal = ({ children, title, handleCloseModal, openModal }: IModal) => {
-  return openModal ? (
+const Modal = ({ title, children, openModal, handleCloseModal }: IModal) => {
+  const portalRoot = document.querySelector('#portal-modal-root') as Element;
+
+  if (!openModal) return null;
+
+  return ReactDOM.createPortal(
     <S.Overlay>
       <S.Wrapper>
         <header>
           <p>{title}</p>
-          <MdClose
-            size={20}
-            color="#666360"
-            onClick={handleCloseModal}
-          />
+          <MdClose size={20} color="#666360" onClick={handleCloseModal} />
         </header>
 
         <S.ModalWrapper>{children}</S.ModalWrapper>
       </S.Wrapper>
-    </S.Overlay>
-  ) : null;
+    </S.Overlay>,
+    portalRoot,
+  );
 };
 
 export default Modal;
