08b924425b77862ee10eec96e9d2b2e2efee039f
Gabriel Ferreira <gabriel.ferreira.itba@gmail.com>
2021-12-04 17:28:52 -0300

    feat: formulário NewTodo testes completos

---
+++ src/components/TodoList/components/Forms/NewTodo/test.spec.tsx
@@ -0,0 +1,38 @@
+import userEvent from '@testing-library/user-event';
+import { render, screen, waitFor } from 'utils/test-utils';
+
+import NewTodo from '.';
+
+describe('<NewTodo />', () => {
+  it('should render NewTodo correctly', () => {
+    render(<NewTodo userId="idtest" onCloseModal={() => ({})} />);
+
+    expect(screen.getByLabelText(/título/i)).toBeInTheDocument();
+    expect(screen.getByLabelText(/descrição/i)).toBeInTheDocument();
+    expect(screen.getByRole('button', { name: /salvar/i })).toBeInTheDocument();
+  });
+
+  it('should render NewTodo correctly with message erros on textbox', async () => {
+    render(<NewTodo userId="idtest" onCloseModal={() => ({})} />);
+
+    userEvent.click(screen.getByRole('button', { name: /salvar/i }));
+
+    await waitFor(() => {
+      expect(screen.getByText(/o título é obrigatório!/i)).toBeInTheDocument();
+    });
+  });
+
+  it('You must send data to the backend and close the formrm', async () => {
+    const onCloseModal = jest.fn();
+    render(<NewTodo userId="idtest" onCloseModal={onCloseModal} />);
+
+    userEvent.type(screen.getByLabelText(/título/i), 'criar novo item');
+    userEvent.type(screen.getByLabelText(/descrição/i), 'item de teste');
+
+    userEvent.click(screen.getByRole('button', { name: /salvar/i }));
+
+    await waitFor(() => {
+      expect(onCloseModal).toHaveBeenCalled();
+    });
+  });
+});
