07f0fda2d99dde556d7e23ea8cf87d20e92eabf1
Gabriel Ferreira <gabriel.ferreira.itba@gmail.com>
2021-12-04 11:35:19 -0300

    feat: componente Input testes completos

---
+++ src/components/Input/test.spec.tsx
@@ -0,0 +1,39 @@
+import userEvent from '@testing-library/user-event';
+import { render, screen } from 'utils/test-utils';
+
+import Input from '.';
+
+describe('<Input />', () => {
+  it('should render the Input correctly', () => {
+    const { container } = render(
+      <Input name="email" error="" placeholder="Digite seu e-mail" />,
+    );
+
+    expect(
+      screen.getByPlaceholderText(/digite seu e-mail/i),
+    ).toBeInTheDocument();
+
+    expect(container.firstChild).toMatchSnapshot();
+  });
+
+  it('should write in the input', async () => {
+    render(<Input name="email" error="" placeholder="Digite seu e-mail" />);
+
+    userEvent.type(
+      screen.getByPlaceholderText(/digite seu e-mail/i),
+      'Testes finalizados',
+    );
+
+    expect(screen.getByPlaceholderText(/digite seu e-mail/i)).toHaveValue(
+      'Testes finalizados',
+    );
+  });
+
+  it('should render erros', async () => {
+    render(<Input name="email" error="Digite o e-mail corretamente" />);
+
+    expect(
+      screen.getByText(/digite o e-mail corretamente/i),
+    ).toBeInTheDocument();
+  });
+});
