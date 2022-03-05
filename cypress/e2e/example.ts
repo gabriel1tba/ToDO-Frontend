32afa8a8156d575b4f22bbc8d336d607ed5fdedb
Gabriel Ferreira <gabriel.ferreira.itba@gmail.com>
2022-03-05 14:40:20 -0300

    feat: configuração typescript ao cypress

---
+++ cypress/e2e/example.ts
@@ -0,0 +1,8 @@
+/* eslint-disable @typescript-eslint/triple-slash-reference */
+/// <reference path="../support/index.d.ts" />
+
+describe('Cypress TS', () => {
+  it('should visit google', () => {
+    cy.google();
+  });
+});
