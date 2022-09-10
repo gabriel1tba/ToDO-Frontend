a923605c7362ae1f958f07f8f72e918cfdb8c463
Gabriel Ferreira <gabrielferreira@pecege.com>
2022-09-10 08:57:07 -0300

    feat: configurando provider de temas no storybook

---
+++ .storybook/preview.js
@@ -0,0 +1,23 @@
+import { ThemeProvider } from 'styled-components';
+
+import theme from '../src/styles/themes/light';
+import GlobalStyles from '../src/styles/global';
+
+export const parameters = {
+  actions: { argTypesRegex: '^on[A-Z].*' },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+};
+
+export const decorators = [
+  (Story) => (
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <Story />
+    </ThemeProvider>
+  ),
+];
